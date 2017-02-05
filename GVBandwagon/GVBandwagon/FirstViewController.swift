3da6073beac6471074ddfc5696e9bbef9379b650
Ryan Korteway <RyanKorteway@me.com>
2017-02-05 18:33:28 -0500

    Final push/commit for the night. Did some final tweaking on the project to make it so that the pop ups were actually showing the riders names when the pop ups asked the drivers if they wanted to give a ride to that person.

--- GVBandwagon/GVBandwagon/FirstViewController.swift
+++ GVBandwagon/GVBandwagon/FirstViewController.swift
@@ -60,27 +60,31 @@ class FirstViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
                     
                         driverRef.child("rider_uid").observeSingleEvent(of: .value, with: { (snapshot) in
                             
-                            let user = self.ref.child("users/\(snapshot.value!)").queryEqual(toValue: "name")
-                            
-                            let alert = UIAlertController(title:"Rider Found", message:"Would you like to give \(user) a ride?.",
-                                preferredStyle: .actionSheet)
-                            
-                            let yesAction = UIAlertAction(title:"Yes", style:.default) { action -> Void in
-                                //code for the action can go here. so accepts or deny's
-                                self.ref.child("users/\(snapshot.value!)/rider/driver_uid").setValue(self.currentUser!.uid)
-                                self.ref.child("users/\(snapshot.value!)/rider/driver_found").setValue(true)
-                            }
-                            
-                            let noAction = UIAlertAction(title:"No", style:.default) { action -> Void in
-                                //code for the action can go here. so accepts or deny's
-                                self.ref.child("users/\(snapshot.value!)/rider/driver_uid").setValue("none")
-                                self.ref.child("users/\(snapshot.value!)/rider/driver_found").setValue(true)
-                            }
-                            
-                            alert.addAction(yesAction)
-                            alert.addAction(noAction)
-                            self.present(alert, animated:true, completion:nil)
-                            driverRef.child("rider_found").setValue(false)
+                            //let user = self.ref.child("users/\(snapshot.value!)").queryEqual(toValue: "name")
+                            self.ref.child("users/\(snapshot.value!)/name").observeSingleEvent(of: .value , with: { (innerSnapshot) in
+                                //ALL OF THIS WORKS OTHER THAN THE QUERY NOT BRINGING UP THE RIDERS ACTUAL NAME!!!
+                                
+                                let alert = UIAlertController(title:"Rider Found", message:"Would you like to give \(innerSnapshot.value!) a ride?",
+                                    preferredStyle: .actionSheet)
+                                
+                                let yesAction = UIAlertAction(title:"Yes", style:.default) { action -> Void in
+                                    //code for the action can go here. so accepts or deny's
+                                    self.ref.child("users/\(snapshot.value!)/rider/driver_uid").setValue(self.currentUser!.uid)
+                                    self.ref.child("users/\(snapshot.value!)/rider/driver_found").setValue(true)
+                                }
+                                
+                                let noAction = UIAlertAction(title:"No", style:.default) { action -> Void in
+                                    //code for the action can go here. so accepts or deny's
+                                    self.ref.child("users/\(snapshot.value!)/rider/driver_uid").setValue("none")
+                                    self.ref.child("users/\(snapshot.value!)/rider/driver_found").setValue(true)
+                                }
+                                
+                                alert.addAction(yesAction)
+                                alert.addAction(noAction)
+                                self.present(alert, animated:true, completion:nil)
+                                driverRef.child("rider_found").setValue(false)
+                                
+                            })
 
                         })
                 }
